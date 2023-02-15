5999b7f933219fdaeee1fba37d1c699b219cb806
baradmitrij <baradmitrij@yandex.ru>
2023-02-16 00:12:19 +0200

    feat: add another experiment with computed and reaction

--- template/index.js
+++ template/index.js
@@ -1,3 +1,6 @@
+// EXPERIMENT 1 --> DRAW.IO PAGES: `Page1`, `toggle`
+
+/*
 const makeObservable = mobx.makeObservable;
 const observable = mobx.observable;
 const action = mobx.action;
@@ -25,3 +28,41 @@ const myTodo = new Todo('one');
 console.dir(myTodo);
 
 myTodo.toggle();
+*/
+
+// EXPERIMENT 2 --> DRAW.IO PAGES: `computed`
+
+const observable = mobx.observable;
+const computed = mobx.computed;
+const reaction = mobx.reaction;
+const runInAction = mobx.runInAction;
+
+const man = {
+  name: 'Ivan',
+  surname: 'Ivanov',
+  get fullname() {
+    return `${this.name} ${this.surname}`;
+  },
+  changeName(newName) {
+    this.name = newName;
+  },
+  changeSurname(newSurname) {
+    this.surname = newSurname;
+  }
+};
+
+const observableMan = observable(man)
+
+reaction(
+  () => `${observableMan.fullname}!!!`,
+  data => console.log(data)
+)
+
+runInAction(() => {
+  observableMan.changeName('Petya');
+  observableMan.changeSurname('Petrov')
+})
+
+
+
+
